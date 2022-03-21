8fdf5ef6195809dbba1529a9c7ae67120631621c
Untiforgins <102057858+Untiforgins@users.noreply.github.com>
2022-03-21 17:27:02 -0400

    Create untiforgins-stats-mod1.py

---
+++ untiforgins-stats-mod1.py
@@ -0,0 +1,35 @@
+import discord
+from datetime import datetime
+
+
+class StatsMod:
+    def __init__(self, country: str, updated: int, uuid: int, flag: str, created: str, lan: int, pop: int, factory: int, land: int, emp: str, rank: str, score: float, competitiveRank: str, gdp: int, balance: int, currency: str, coinz: int, tech: int, keys: int, badge1: str, badge2: str, badge3: str):
+        self.country = country
+        self.updated = updated
+        self.uuid = uuid
+        self.flag = flag
+        self.created = created
+        self.lan = lan
+        self.pop = pop
+        self.factory = factory
+        self.land = land
+        self.emp = emp
+        self.rank = rank
+        self.score = score
+        self.competitiveRank = competitiveRank
+        self.gdp = gdp
+        self.balance = balance
+        self.currency = currency
+        self.coinz = coinz
+        self.tech = tech
+        self.keys = keys
+        self.badge1 = badge1
+        self.badge2 = badge2
+        self.badge3 = badge3
+
+    def UserMod(self):
+        embed = discord.Embed(title="Stats", colour=ctx.author.colour, timestamp=ctx.message.created_at)
+        embed.set_thumbnail(url=self.flag)
+        embed.set_footer(text="Israel is rightful Jewish land, Palestine doesnt exist.")
+        embed.set_author()
+        return embed
